3c6154e70e7959d1da95a600024d72c0aa86d701
Ev-200 <1269@iai.pt>
2025-12-09 08:45:55 +0000

    html

---
+++ Ex-número_1/app.js
@@ -0,0 +1,11 @@
+let popuptexto = "olá mundo"
+alert(popuptexto)
+
+let nome = prompt("escreve o teu nome")
+console.log("o teu nome é: " + nome)
+// alert("olá " + nome + "!")
+alert(`olá ${nome.toUpperCase()}!`)
+alert(`olá ${nome.trim()}!`)
+
+alert(`olá ${nome.indexOf("m")}!`)
+let char = nome[0]
