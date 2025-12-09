fdace966a2918b2842e9f6294873953e15b59592
Ev-200 <1269@iai.pt>
2025-12-09 11:38:44 +0000

    ex 5

--- Ex-número_5/app.js
+++ Ex-número_5/app.js
@@ -1,7 +1,7 @@
 let numeroSorte = Math.random() * 10 + 1
 numeroSorte = Math.floor(numeroSorte)
 console.log(numeroSorte)
-
+alert("Olá Mundo! Adivinha o número em que estou a pensar. Tens 6 tentativas!")
 let adivinhar = prompt("Adivinha o número da sorte, entre 1 e 10")
 adivinhar = Math.floor(Number(adivinhar))
 
@@ -20,25 +20,33 @@ if (adivinhar > 10 || adivinhar <= 0) {
         if (numeroSorte === adivinhar) {
             alert("Certo!!")
             ganhou = true
+        } else if (ntentativas == 5) {
+            ntentativas = ntentativas + 1
+            alert("Última tentativa")
+
+        } else if (ntentativas == 7) {
+            ganhou = true
+            alert("Perdeste")
         } else if (numeroSorte > adivinhar && numeroSorte - adivinhar <= 2 || numeroSorte < adivinhar && adivinhar - numeroSorte <= 2) {
             alert("Quase lá!!")
-
+            alert("Ainda tens " + (7 - Number(ntentativas)) + " tentativas")
             adivinhar = prompt("Adivinha o número da sorte, entre 1 e 10")
             ntentativas = ntentativas + 1
             console.log(ntentativas)
             adivinhar = Math.floor(Number(adivinhar))
         } else if (numeroSorte > adivinhar) {
             alert("muito abaixo")
-
+            alert("Ainda tens " + (7 - Number(ntentativas)) + " tentativas")
+            ntentativas = ntentativas + 1
             adivinhar = prompt("Adivinha o número da sorte, entre 1 e 10")
             adivinhar = Math.floor(Number(adivinhar))
         } else if (numeroSorte < adivinhar) {
             alert("muito acima")
-
+            alert("Ainda tens " + (7 - Number(ntentativas)) + " tentativas")
+            ntentativas = ntentativas + 1
             adivinhar = prompt("Adivinha o número da sorte, entre 1 e 10")
             adivinhar = Math.floor(Number(adivinhar))
-        } else if (ntentativas == 5) {
-            alert("Última ")
+
         }
     }
 }
