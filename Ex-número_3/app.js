00c9912709313b9b3d2366caba0bea80f0910407
Ev-200 <1269@iai.pt>
2025-12-09 09:47:00 +0000

    ex 3

--- Ex-número_3/app.js
+++ Ex-número_3/app.js
@@ -1,2 +1,4 @@
 alert("Olá Mundo! Vamos imprimir na console uma tabuada tradicional!")
 let numero = prompt("Insirir um número")
+
+console.log(`${numero * 1} ${numero * 2} ${numero * 3} ${numero * 4} ${numero * 5} ${numero * 6} ${numero * 7} ${numero * 8} ${numero * 9} ${numero * 10}`)
