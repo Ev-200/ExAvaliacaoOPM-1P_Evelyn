cd6cf81dbe9118102a709d6c40652476539f100a
Ev-200 <1269@iai.pt>
2025-12-09 09:57:43 +0000

    ex 2

--- Ex-número_2/app.js
+++ Ex-número_2/app.js
@@ -4,4 +4,11 @@ let numero = prompt("Inserir um número")
 Number(numero)
 console.log(numero)
 
+if (numero % 2 == 0) {
+    alert("O teu número é par")
+} else if (numero % 2 != 0); {
+    alert("O teu número é ímpar")
+}
+
+
 
