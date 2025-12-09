271dea210802a949681d29d2f8af6ca8cc454669
Ev-200 <1269@iai.pt>
2025-12-09 12:36:33 +0000

    ex 6

--- Ex-número_6/app.js
+++ Ex-número_6/app.js
@@ -4,8 +4,19 @@ let operacao = prompt("Insirir uma operação: soma, subtração, multiplicaçã
 if (operacao == "+" || "-" || "*" || "/") {
     let nesq = prompt(("inserir o número à esquerda do sinal de ") + (operacao))
     let ndir = prompt(("inserir o número à direta do sinal de ") + (operacao))
-    alert(Number(nesq) + Number(ndir))
-    console.log(Number(nesq) + Number(ndir))
-} else if (operacao != "+" || "-" || "*" || "/") {
-    alert("Operação inválida")
+    if (operacao == "+") {
+        alert(Number(nesq) + Number(ndir))
+        console.log(Number(nesq) + Number(ndir))
+    } else if (operacao == "-") {
+        alert(Number(nesq) - Number(ndir))
+        console.log(Number(nesq) - Number(ndir))
+    } else if (operacao == "*") {
+        alert(Number(nesq) * Number(ndir))
+        console.log(Number(nesq) * Number(ndir))
+    } else if (operacao == "/") {
+        alert(Number(nesq) / Number(ndir))
+        console.log(Number(nesq) / Number(ndir))
+    } else if (operacao != "+" || "-" || "*" || "/") {
+        alert("Operação inválida")
+    }
 }
