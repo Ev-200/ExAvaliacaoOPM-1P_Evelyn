d0bfc3602dfbae09b8734e75b2ce7ea42bd59885
Ev-200 <1269@iai.pt>
2025-12-09 12:18:04 +0000

    ex 6

---
+++ Ex-número_6/app.js
@@ -0,0 +1,11 @@
+alert("Olá Mundo! Vamos fazer contas simples.")
+let operacao = prompt("Insirir uma operação: soma, subtração, multiplicação ou divisão")
+
+if (operacao == "+" || "-" || "*" || "/") {
+    let nesq = prompt(("inserir o número à esquerda do sinal de ") + (operacao))
+    let ndir = prompt(("inserir o número à direta do sinal de ") + (operacao))
+    alert(Number(nesq) + Number(ndir))
+    console.log(Number(nesq) + Number(ndir))
+} else if (operacao != "+" || "-" || "*" || "/") {
+    alert("Operação inválida")
+}
